9af42b98914d18287ff306462539b2789ee91ee9
Efremov Egor <efremovegor936@gmail.com>
2023-02-19 21:17:07 +0300

    .

--- requests_test.py
+++ requests_test.py
@@ -16,5 +16,7 @@ with open("test.txt", "r") as f:
     for row in f.readlines():
         parts = row.split(":")
         data[parts[0].strip()] = parts[1].strip() if parts[1].strip() else ""
-
+data["csrf_token"] = json.loads(html_objects.find("body")["data-app-data"])[
+    "csrf-token"
+]
 print(session.post("https://funpay.com/chips/saveOffers", data=data))
