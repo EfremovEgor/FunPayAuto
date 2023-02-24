336578b8238bb32645aaf53e5479605a2a89440b
Efremov Egor <efremovegor936@gmail.com>
2023-02-24 20:36:50 +0300

    calc fix

--- price_calc.py
+++ price_calc.py
@@ -3,8 +3,8 @@ price_out = 1232179226069246418772385351098853723957544392402548031488
 
 
 def get_final_price(initial_price: float):
-    return round(initial_price * (price_out / price_in), 3)
+    return round(initial_price * (price_out / price_in), 2)
 
 
 def get_initial_price(final_price: float):
-    return round(final_price / (price_out / price_in), 3)
+    return round(final_price / (price_out / price_in), 2)
