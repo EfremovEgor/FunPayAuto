caa1fe71c5c127c7a651fc892d849ffc86aff106
Efremov Egor <efremovegor936@gmail.com>
2023-02-26 23:26:27 +0300

    redundant

--- requests_worker.py
+++ requests_worker.py
@@ -40,19 +40,6 @@ def form_payload(data: dict = None) -> dict:
     return payload
 
 
-def update_phpsessid(session: requests.Session) -> requests.Session:
-    ...
-
-
-# {
-#     "golden_key": "jdep2eq02izctj4lh4ljyog20qiwojj2",
-#     "_ym_isad": "2",
-#     "_ym_d": "1677413639",
-#     "_gid": "GA1.2.267647961.1677413639",
-#     "_ga": "GA1.2.1289151014.1677413639",
-#     "_ym_uid": "1677413639953450181",
-#     "PHPSESSID": "jz4iwR33sdUw90-V3m2lLIao-oaX4Mrw"
-# }
 def send_request(payload: dict) -> int:
     session = requests.Session()
     session.headers = {"User-Agent": UserAgent().random}
